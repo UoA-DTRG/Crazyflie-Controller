09d7f523f04180a130ce446b0d16b942bc2b44ae
Jaap Skinner <jaap.kinne@hotmail.co.nz>
2024-08-05 15:07:17 +1200

    tracking working tested with mug

--- vicon_test.py
+++ vicon_test.py
@@ -1,7 +1,9 @@
+import time
 import pyvicon_datastream as pv
+from pyvicon_datastream import tools
 
-VICON_TRACKER_IP = "10.0.108.3"
-OBJECT_NAME = "My_Object"
+VICON_TRACKER_IP = "192.168.10.1"
+OBJECT_NAME = "mug"
 
 vicon_client = pv.PyViconDatastream()
 ret = vicon_client.connect(VICON_TRACKER_IP)
@@ -11,20 +13,8 @@ if ret != pv.Result.Success:
 else:
     print(f"Connection to {VICON_TRACKER_IP} successful")
 
-
-
-
-
-
-
-
-# from pyvicon_datastream import tools
-
-# VICON_TRACKER_IP = "10.0.108.3"
-# OBJECT_NAME = "My_Object"
-
-# mytracker = tools.ObjectTracker(VICON_TRACKER_IP)
-# while(True):
-#     position = mytracker.get_position(OBJECT_NAME)
-#     print(f"Position: {position}")
-#     time.sleep(0.5)
+mytracker = tools.ObjectTracker(VICON_TRACKER_IP)
+while(True):
+    position = mytracker.get_position(OBJECT_NAME)
+    print(f"Position: {position}")
+    time.sleep(0.5)
