cab177f9a7ce6ee31372dae506095e641033f9f2
jaapskinner <jaap.skinner@hotmail.co.nz>
2024-08-09 12:41:41 +1200

    working on crazyflie controller running in new thread??

--- app/crazyflie_connection.py
+++ app/crazyflie_connection.py
@@ -1,12 +1,23 @@
 import time
 import threading
-import cflib.crtp# type: ignore
-from cflib.crazyflie.swarm import CachedCfFactory# type: ignore
-from cflib.crazyflie.swarm import Swarm# type: ignore
+import cflib.crtp # type: ignore
+from cflib.crazyflie.swarm import CachedCfFactory # type: ignore
+from cflib.crazyflie.swarm import Swarm # type: ignore
 
 class CrazyflieConnection:
     def __init__(self, uris):
         self.uris = uris
+        self.swarm = None
+
+    def update_uris(self, uris):
+        self.uris = uris
+
+    def connect(self):
+        cflib.crtp.init_drivers()
+        factory = CachedCfFactory(rw_cache='./cache')
+        self.swarm = Swarm(self.uris, factory=factory)
+        self.swarm.parallel_safe(self.light_check)
+        self.swarm.reset_estimators()
 
     def light_check(self, scf):
         scf.cf.param.set_value('led.bitmask', 255)
@@ -14,26 +25,5 @@ class CrazyflieConnection:
         scf.cf.param.set_value('led.bitmask', 0)
         time.sleep(2)
 
-    def take_off(self, scf):
-        commander= scf.cf.high_level_commander
-
-        commander.takeoff(1.0, 2.0)
-        time.sleep(3)
-
-    def land(self, scf):
-        commander= scf.cf.high_level_commander
-
-        commander.land(0.0, 2.0)
-        time.sleep(2)
-
-        commander.stop()
-    
-    def run(self):
-        def run_swarm():
-            cflib.crtp.init_drivers()
-            factory = CachedCfFactory(rw_cache='./cache')
-            with Swarm(self.uris, factory=factory) as swarm:
-                swarm.parallel_safe(self.light_check)
-                swarm.reset_estimators()
-        thread = threading.Thread(target=run_swarm)
-        thread.start()
+    def disconnect(self):
+        self.swarm.close_links()
